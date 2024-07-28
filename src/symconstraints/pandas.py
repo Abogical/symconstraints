42ed377446f749f98f9a41299ab406379a48344e
Abdelrahman Abdelrahman <git@abogic.al>
2024-07-28 02:59:10 -0600

    feat(pandas): Add check function

--- src/symconstraints/pandas.py
+++ src/symconstraints/pandas.py
@@ -1,13 +1,21 @@
 """Integration with [Pandas](https://pandas.pydata.org/) to aid in data cleaning dataframes."""
 
 import pandas
-from sympy import Symbol
+import math
+from sympy import Symbol, lambdify
 from pandas.api.types import (
     is_unsigned_integer_dtype,
     is_integer_dtype,
     is_float_dtype,
     is_complex_dtype,
 )
+from functools import cache
+
+from symconstraints import Constraints, Validation
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from sympy.logic.boolalg import Boolean
 
 
 def symbols(
@@ -94,3 +102,113 @@ def symbols(
             return result[0]
 
     return result
+
+
+@cache
+def _lambdify(*args, **kwargs):
+    return lambdify(*args, **kwargs, modules=["numpy"])
+
+
+def check(
+    constraints: Constraints | Validation, df: pandas.DataFrame
+) -> pandas.DataFrame:
+    """Return a table checking all the validations provided.
+
+    Parameters
+    ----------
+    constraints : Constraints | Validation
+        `Constraints` or `Validation` to use for checking.
+    df : pandas.DataFrame
+        Dataframe to check
+
+    Returns
+    -------
+    pandas.DataFrame
+        Returns a dataframe showing the result of all the validations for each row in the dataframe.
+        Each column in the dataframe corresponds to each validation operation provided. Each row
+        corresponds to each row in the original dataframe. A result of 1 is shown for successful validation,
+        0 for an unsuccessful validation, and NaN for validations that can't be computed due to missing values.
+        The result dtype is `float16`.
+
+        If a `Constraints` object is provided, the columns are `pandas.MultiIndex`, where the top level is a set of
+        columns with all validations concerning those sets of columns under it.
+
+    Examples
+    --------
+    >>> import pandas as pd
+    >>> from symconstraints.pandas import symbols, check
+    >>> from symconstraints import Constraints
+    >>> from sympy import Eq
+    >>> df = pd.DataFrame({
+    ...     'A': [5,6,8,9],
+    ...     'B': [3,5,90,None],
+    ...     'C': [14, 30, None, None]
+    ... }, dtype=float)
+    >>> A, B, C = symbols(df, ['A', 'B', 'C'])
+    >>> constraints = Constraints([A > B, Eq(C, B*A)])
+    >>> check(constraints, df)
+    # Order may differ
+      (A, B)  (C, A, B)  (C, B)  (C, A)
+       A > B Eq(C, A*B) B < C/B A > C/A
+    0    1.0        0.0     1.0     1.0
+    1    1.0        1.0     1.0     1.0
+    2    0.0        NaN     NaN     NaN
+    3    NaN        NaN     NaN     NaN
+    """
+    if isinstance(constraints, Validation):
+        columns = tuple(constraints.keys)
+        index: list[Boolean] = list(constraints.operations)
+        columns_str = [str(column) for column in columns]
+
+        relevant_indices = ~df[columns_str].isna().any(axis="columns")
+        if not isinstance(relevant_indices, pandas.Series):
+            raise RuntimeError(
+                f"unexpected type for relevant_indices: {type(relevant_indices)}"
+            )
+
+        relevant_df = df[relevant_indices]
+        # Return a dataframe if the output is a series
+        return pandas.DataFrame(
+            pandas.concat(
+                [
+                    pandas.concat(
+                        dict(
+                            (
+                                operation,
+                                _lambdify(columns, operation)(
+                                    *(relevant_df[str(column)] for column in columns)
+                                ).astype("float16"),
+                            )
+                            for operation in index
+                        ),
+                        axis="columns",
+                    ),
+                    pandas.concat(
+                        dict(
+                            (
+                                operation,
+                                pandas.Series(
+                                    math.nan,
+                                    index=relevant_indices.index[~relevant_indices],
+                                    copy=True,
+                                ),
+                            )
+                            for operation in index
+                        ),
+                        axis="columns",
+                    ),
+                ],
+                axis="rows",
+            )
+        )
+
+    if isinstance(constraints, Constraints):
+        return pandas.concat(
+            dict(
+                (validation.keys, check(validation, df))
+                for validation in constraints.validations
+            ),
+            axis="columns",
+        )
+
+    raise ValueError(f"Invalid constraints given: {constraints}")
--- tests/symconstraints/test_pandas.py
+++ tests/symconstraints/test_pandas.py
@@ -1,7 +1,11 @@
 from symconstraints import pandas as sympd
+from symconstraints import Constraints, Validation
+from sympy import Eq
 import pandas as pd
 import unittest
 import doctest
+import math
+from collections import Counter
 
 
 def test_symbols():
@@ -22,5 +26,87 @@ def test_symbols():
     )
 
 
+def test_check():
+    df = pd.DataFrame(
+        {
+            "height": [5, 6, 8, 9],
+            "width": [3, 5, 90, None],
+            "area": [14, 30, None, None],
+        },
+        dtype=float,
+    )
+
+    symbols = sympd.symbols(df, ["height", "width", "area"])
+    assert isinstance(symbols, list)
+    height, width, area = symbols
+
+    validation = Validation(frozenset([height, width]), frozenset([height > width]))
+
+    check_result = sympd.check(validation, df)
+    check_comparision = check_result.compare(
+        pd.DataFrame({(height > width): [1.0, 1.0, 0.0, math.nan]}, dtype="float16")
+    )
+
+    assert check_comparision.empty, check_comparision
+
+    constraints = Constraints([height > width, Eq(area, width * height)])
+
+    check_result = sympd.check(constraints, df)
+    assert len(check_result.columns) == 4
+    check_comparision = check_result.compare(
+        pd.DataFrame(
+            pd.DataFrame(
+                {
+                    (frozenset([height, width]), (height > width)): [
+                        1.0,
+                        1.0,
+                        0.0,
+                        math.nan,
+                    ],
+                    (frozenset([area, height, width]), Eq(area, width * height)): [
+                        0.0,
+                        1.0,
+                        math.nan,
+                        math.nan,
+                    ],
+                    (frozenset([width, area]), width < area / width): [
+                        1.0,
+                        1.0,
+                        math.nan,
+                        math.nan,
+                    ],
+                    (frozenset([height, area]), height > area / height): [
+                        1.0,
+                        1.0,
+                        math.nan,
+                        math.nan,
+                    ],
+                },
+                dtype="float16",
+            )[check_result.columns]
+        )
+    )
+
+    assert check_comparision.empty, check_comparision
+
+
+class OutputChecker(doctest.OutputChecker):
+    def check_output(self, want, got, optionflags):
+        # Don't care about exact order of output as it is not deterministic
+        want_lines, got_lines = list(want.splitlines()), list(got.splitlines())
+        if len(want_lines) > 0 and "Order may differ" in want_lines[0]:
+            del want_lines[0]
+            return all(
+                Counter(want_line) == Counter(got_line)
+                for want_line, got_line in zip(want_lines, got_lines)
+            )
+
+        return super().check_output(want, got, optionflags)
+
+
 def test_docs():
-    assert unittest.TextTestRunner().run(doctest.DocTestSuite(sympd)).wasSuccessful()
+    assert (
+        unittest.TextTestRunner()
+        .run(doctest.DocTestSuite(sympd, checker=OutputChecker()))
+        .wasSuccessful()
+    )
