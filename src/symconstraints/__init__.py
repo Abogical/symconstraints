e0c33582b19602c270b6ac9c50492aac5a50282a
Abdelrahman Abdelrahman <git@abogic.al>
2024-08-02 23:44:24 -0600

    docs: Add package level example

--- src/symconstraints/__init__.py
+++ src/symconstraints/__init__.py
@@ -10,7 +10,36 @@ your dataset with little code.
 
 Examples
 --------
-TODO
+Validate and impute a dataframe
+>>> import pandas as pd
+>>> from symconstraints import Constraints
+>>> from symconstraints.pandas import symbols, check, set_invalid_all, impute
+>>> from sympy import Eq
+>>> df = pd.DataFrame(
+...    {
+...         "height": [5, 6, 8, 9],
+...         "width": [3, 5, 7, None],
+...         "area": [14, 30, None, 18],
+...     },
+...     dtype=float,
+... )
+>>> height, width, area = symbols(df, ["height", "width", "area"])
+>>> constraints = Constraints([height > width, Eq(area, width * height)])
+>>> check_result = check(constraints, df)
+>>> df = set_invalid_all(check_result, df)
+>>> df
+    height  width  area
+0     NaN    NaN   NaN
+1     6.0    5.0  30.0
+2     8.0    7.0   NaN
+3     9.0    NaN  18.0
+>>> imputed_df = impute(constraints, df)
+>>> imputed_df
+    height  width  area
+0     NaN    NaN   NaN
+1     6.0    5.0  30.0
+2     8.0    7.0  56.0
+3     9.0    2.0  18.0
 """
 
 from symconstraints.constraints import Constraints, symbols
